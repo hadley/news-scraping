c945b127f353ca0d79752e5f637ecd9f1e157b63
Hadley Wickham <h.wickham@gmail.com>
2025-09-16 14:33:43 -0500

    Initial commit

---
+++ scrape.R
@@ -0,0 +1,12 @@
+library(httr2)
+
+req <- request("https://newsapi.org/v2/everything") |>
+  req_url_query(
+    q = '`"data science"`',
+    from = Sys.Date() - 1,
+    pageSize = 10,
+    apiKey = Sys.getenv("NEWS_API_KEY")
+  )
+
+dir.create("data", showWarnings = FALSE)
+req_perform(req, path = paste0("data/", Sys.Date(), ".json"))
